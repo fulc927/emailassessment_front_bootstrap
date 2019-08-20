240b746768dd799dc8f63033ebae908e509a70f9
fulc927 <fulc927@opentelecom.fr>
2019-08-20 11:41:48 +0200

    Update consume.php

--- consume.php
+++ consume.php
@@ -12,7 +12,8 @@ $callback_func = function(AMQPEnvelope $message, AMQPQueue $queue) use (&$max_jo
 	$queue->ack($message->getDeliveryTag());
 	global $i;
 	if(isset($_POST['action']) && !empty($_POST['action'])) {
-        echo json_encode($i);
+        //echo json_encode($i);
+		echo "Message $i: ";
          }
 
         //echo "Message $i: " . $message->getBody() . "\n";
