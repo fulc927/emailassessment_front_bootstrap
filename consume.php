320c7e0fbb9933462a28d691137ddec74136025b
fulc927 <fulc927@opentelecom.fr>
2019-08-24 14:56:32 +0200

    Update consume.php

--- consume.php
+++ consume.php
@@ -14,12 +14,15 @@ if (isset($_SESSION['key']) && !empty($_SESSION['key'])) {
         //echo json_encode($message->getBody()). "\n";
         echo "Message $i: " . $message->getBody() . "\n";
         $i++;
-        if ($i = 1) {
+        if ($i = 0) {
             // Bail after 1 message
 		$_SESSION['key'] = '';
                 return false;
-        }else {  
-	$connection->disconnect();
+        }
+	else {  
+		// Bail after 1 message
+		$_SESSION['key'] = '';
+                return false;
         }
 	};
 	try{
